493976c6175eab44cc4965dfa36ec10d349082df
Jordan Maples <jordan.d.maples@gmail.com>
2022-04-17 14:16:13 -0500

    Added lazy instantiation for Line Graph

--- chartsDemo2/SecondViewController.swift
+++ chartsDemo2/SecondViewController.swift
@@ -5,17 +5,58 @@
 //  Created by Jordan Maples on 4/14/22.
 //
 
+import Charts
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, ChartViewDelegate {
+    
+    lazy var lineChart: LineChartView = {
+        let chartView = LineChartView()
+        
+        chartView.backgroundColor = .systemFill
+        
+        return chartView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        lineChart.delegate = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        lineChart.frame = CGRect(x: 0, y: 0,
+                                width: self.view.frame.size.width,
+                               height: self.view.frame.size.width)
+        lineChart.center = view.center
+        view.addSubview(lineChart)
+        
+        
+        
+        var entries = [ChartDataEntry]()
+        
+        for x in 0..<10 {
+            entries.append(ChartDataEntry(x: Double(x),
+                                             y: Double(x))
+                           )
+        }
+        
+        let set = LineChartDataSet(entries: entries, label: "Day")
+        set.colors = ChartColorTemplates.material()
 
+        let data = LineChartData(dataSet: set)
+        lineChart.data = data
+
+    }
+    
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print(entry)
+    }
+
+    
 
 
 }
--- chartsDemo2/ViewController.swift
+++ chartsDemo2/ViewController.swift
@@ -5,13 +5,42 @@
 //  Created by Jordan Maples on 4/14/22.
 //
 
+import Charts
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChartViewDelegate {
+    
+    var barChart = BarChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        barChart.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        barChart.frame = CGRect(x: 0, y: 0,
+                                width: self.view.frame.size.width,
+                               height: self.view.frame.size.width)
+        barChart.center = view.center
+        view.addSubview(barChart)
+        
+        var entries = [BarChartDataEntry]()
+        
+        for x in 0..<10 {
+            entries.append(BarChartDataEntry(x: Double(x),
+                                             y: Double(x))
+                           )
+        }
+        
+        let set = BarChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.joyful()
+
+        let data = BarChartData(dataSet: set)
+        barChart.data = data
+
     }
 
 
